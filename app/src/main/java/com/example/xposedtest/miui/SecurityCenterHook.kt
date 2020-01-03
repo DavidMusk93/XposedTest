708fba606fec63922e5725bab4c5d086ac91e0d2
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-01-03 22:01:15 +0800

    automatic enable install apk while `adb install *.apk`
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- app/src/main/java/com/example/xposedtest/miui/SecurityCenterHook.kt
+++ app/src/main/java/com/example/xposedtest/miui/SecurityCenterHook.kt
@@ -1,9 +1,13 @@
 package com.example.xposedtest.miui
 
+import android.widget.Button
 import com.example.xposedtest.utility.C
 import com.example.xposedtest.utility.cast
+import com.example.xposedtest.utility.getField
 import com.example.xposedtest.xposed.*
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import kotlinx.coroutines.*
 
 class SecurityCenterHook(lpparam: XC_LoadPackage.LoadPackageParam): HookEntry(lpparam, HookContext()), IHookEntry {
 
@@ -13,13 +17,38 @@ class SecurityCenterHook(lpparam: XC_LoadPackage.LoadPackageParam): HookEntry(lp
   }
 
   private fun hook() {
-    "com.miui.common.persistence.RemoteProvider".`class`()!!.hook("hD",
+    "${SecurityCenter.RemoteProvider}".`class`()!!.hook("hD",
         C.String, C.Boolean,
         hookBefore {
           val k = args[0].cast<String>()?: return@hookBefore
           if (k.equals("security_adb_install_enable"))
             args[1] = false
-          log("ModifyArg", "enable adb install")
+          log("BlockPermissionCheck", "enable adb install")
+        })
+
+    "${SecurityCenter.AdbInstallActivity}".`class`()!!.hook("Kt",
+        C.View,
+        hookAfter {
+          val obj = thisObject.getField<Any>("BT")
+          // In kotlin, it is really hard to represent `Class[]`
+          // val clsArr: Array<Class<*>> = Array(1, {IntArray::class.java.componentType})
+          // val cls = Class.forName("com.android.internal.app.AlertController")
+          var acceptButton: Button? = null
+          runCatching {
+            // log("Button(-1)", XposedHelpers.callStaticMethod("${SecurityCenter.a}".`class`(), "bwk", obj, "getButton", cls, clsArr, -1))
+            // log("Button(-2)", XposedHelpers.callStaticMethod("${SecurityCenter.a}".`class`(), "bwk", obj, "getButton", cls, clsArr, -2))
+            log("Button(-1)", XposedHelpers.callMethod(obj, "getButton", -1)) // Same as Button `Ca`
+            log("Button(-2)", XposedHelpers.callMethod(obj, "getButton", -2)
+                .also { it
+                    .cast<Button>()
+                    ?.apply { acceptButton = this } })
+            log("Button(Ca)", thisObject.getField("Ca"))
+          }.onFailure { log("Exception", "${it.message}") }
+          CoroutineScope(Dispatchers.Default).launch {
+            delay(3000)
+            MainScope().launch { acceptButton?.performClick() }
+          }
         })
   }
+
 }
