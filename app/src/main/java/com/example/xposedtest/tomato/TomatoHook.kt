eb8da70037fadf32ccde270aa6a11df40ed85aaf
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-01-07 09:32:01 +0800

    disable AD
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- app/src/main/java/com/example/xposedtest/tomato/TomatoHook.kt
+++ app/src/main/java/com/example/xposedtest/tomato/TomatoHook.kt
@@ -5,11 +5,22 @@ import android.view.View
 import com.example.xposedtest.utility.C
 import com.example.xposedtest.utility.cast
 import com.example.xposedtest.utility.getField
+import com.example.xposedtest.utility.setField
 import com.example.xposedtest.xposed.*
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-class TomatoHookContext: HookContext() {
+class TomatoHookContext : HookContext() {
+
+  companion object {
+    val idMap = mapOf(
+        2131297368 to "find",
+        2131297390 to "live",
+        2131297401 to "mime",
+        2131297468 to "tomato",
+        2131297776 to "game"
+    )
+  }
 
   private lateinit var realClassLoader: ClassLoader
 
@@ -34,10 +45,31 @@ class TomatoHookContext: HookContext() {
     "com.one.tomato.ui.MainTabActivity".`class`()
         .apply {
           hook("onClick", C.View,
-              hookBefore { log("onClick", args[0].cast<View>()?.id) })
+              hookBefore { log("onClick", args[0].cast<View>()?.id?.let { idMap[it] }) })
 
           hook("k", C.Int,
               hookBefore { log("(k)Input", args[0], thisObject.getField("B")) })
+        }
+  }
+
+  private fun hookAd() {
+    "com.one.tomato.utils.f".`class`()
+        .apply {
+          hook("c", C.String, hookAfter { result = null })
+
+          hook("e", C.String, hookAfter { result = null })
+        }
+  }
+
+  private fun hookLookTime() {
+    "ny".`class`()
+        .apply {
+          hook("b", hookAfter { result = 1000 })
+
+          hook("a", hookAfter {
+            thisObject.getField<Any>("f5814a")?.setField("hasPay", true)
+            result = 1000
+          })
         }
   }
 
@@ -52,6 +84,7 @@ class TomatoHookContext: HookContext() {
               ?.apply {
                 realClassLoader = this
                 hookTrivial()
+                hookAd()
               }
         })
 
