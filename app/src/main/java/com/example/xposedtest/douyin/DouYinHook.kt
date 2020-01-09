96e4380096284dac444dfba2bf854aa2d68d8160
Mingqiang Sun <mingqiang.sun@weimob.com>
2020-01-09 21:10:54 +0800

    [douyin] print 'log'
    Signed-off-by: Mingqiang Sun <mingqiang.sun@weimob.com>

--- app/src/main/java/com/example/xposedtest/douyin/DouYinHook.kt
+++ app/src/main/java/com/example/xposedtest/douyin/DouYinHook.kt
@@ -1,18 +1,43 @@
 package com.example.xposedtest.douyin
 
-import com.example.xposedtest.xposed.HookContext
-import com.example.xposedtest.xposed.HookEntry
-import com.example.xposedtest.xposed.IHookEntry
+import com.example.xposedtest.annotation.HookMethod
+import com.example.xposedtest.utility.C
+import com.example.xposedtest.utility.cast
+import com.example.xposedtest.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class DouYinHook(param: XC_LoadPackage.LoadPackageParam) : HookEntry(param, HookContext()), IHookEntry {
 
   override fun setupHook() {
     super.setupHook(javaClass.simpleName)
-    hook()
+    for (method in this::class.java.declaredMethods) {
+      method.getAnnotation(HookMethod::class.java) ?: continue
+      method.invoke(this)
+    }
   }
 
-  private fun hook() {
+  @HookMethod
+  private fun hookVideoDownload() {
+    "kotlin.jvm.internal.Intrinsics".`class`()!!
+        .apply {
+          hook("checkExpressionValueIsNotNull",
+              C.Object, C.String,
+              hookBefore {
+                val k = args[1].cast<String>() ?: return@hookBefore
+                if (k.contains(Regex("url|uri")))
+                  log("checkExpressionValueIsNotNull", *args)
+              })
+        }
 
+    "com.ss.android.ugc.aweme.feed.model.Video".`class`()!!
+        .apply {
+          hook("isHasWaterMark", hookAfter {
+            /**
+             * true, normal video
+             * false, big video
+             */
+            log("WaterMark", result)
+          })
+        }
   }
 }
