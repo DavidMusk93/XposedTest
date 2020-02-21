7f7e2e7672afc4f9906e7630db14354beba93c49
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-02-21 21:26:09 +0800

    set specific app as non-deletable while dragging
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- app/src/main/java/com/example/xposedtest/module/miui/MiuiHomeHook.kt
+++ app/src/main/java/com/example/xposedtest/module/miui/MiuiHomeHook.kt
@@ -2,8 +2,11 @@ package com.example.xposedtest.module.miui
 
 import com.example.xposedtest.annotation.HookMethod
 import com.example.xposedtest.utility.C
+import com.example.xposedtest.utility.callMethod
+import com.example.xposedtest.utility.getField
 import com.example.xposedtest.xposed.*
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.lang.Exception
 
 class MiuiHomeHook(lpparam: XC_LoadPackage.LoadPackageParam)
   : HookEntry(lpparam, HookContext()), IHookEntry {
@@ -14,19 +17,62 @@ class MiuiHomeHook(lpparam: XC_LoadPackage.LoadPackageParam)
 
   @HookMethod
   private fun hookUnintall() {
-    val protectAppList = listOf("com.example.xposedtest")
-
-    val ShortcutInfo = "com.miui.home.launcher.ShortcutInfo".`class`()
-    "com.miui.home.launcher.UninstallDialog".`class`()!!
+    val ShortcutInfo = "$packageName.launcher.ShortcutInfo".`class`()
+    "$packageName.launcher.UninstallDialog".`class`()!!
         .apply {
           hook("deletePackage", C.String, ShortcutInfo,
               hookBefore {
                 log("Uninstall", *args)
-                if (protectAppList.contains("${args[0]}")) {
+                if (Config.Package.protectionAppList.contains("${args[0]}")) {
                   args[0] = null
                   args[1] = null
                 }
               })
         }
+
+    val DragObject="$packageName.launcher.DragObject".`class`()
+    val ItemInfo="$packageName.launcher.ItemInfo".`class`()
+    val CellInfo="$packageName.launcher.CellLayout.CellInfo".`class`()
+
+    "$packageName.launcher.DeleteZone".`class`()!!
+        .apply {
+          //hook("deleteItem",DragObject,ItemInfo,
+          //    hookBefore {
+          //      log("DeleteZone",*args)
+          //      log("ItemType",args[1].getField("itemType"))
+          //    })
+
+          //hook("onDrop",DragObject,
+          //    hookBefore {
+          //      log("CanBeDeleted",thisObject.callMethod("checkDeletedOperationPermission",args[0]))
+          //      log("DragInfo",args[0].callMethod("getDragInfo"))
+          //    })
+
+          hook("checkDeletedOperationPermission",DragObject,
+              hookAfter {
+                log("checkDeletedOperationPermission",*args,result)
+                //kotlin.runCatching {
+                //  throw Exception("trivial")
+                //}.onFailure {
+                //  it.stackTrace.forEach {
+                //    log("StackTrace",it)
+                //  }
+                //}
+                kotlin.runCatching {
+                  log("checkDeletedOperationPermission",args[0]
+                      .callMethod("getDragInfo")
+                      ?.getField<String>("mIconPackage")?.also {
+                        if(Config.Package.protectionAppList.contains(it)){
+                          result=false
+                        }
+                      })
+                }.onFailure { log("GetField@Failure",it) }
+              })
+        }
+
+    //"$packageName.launcher.Launcher".`class`()!!
+    //    .apply {
+    //      hook("onLongClick",C.View, hookBefore { log("onLongClick",*args) })
+    //    }
   }
 }
