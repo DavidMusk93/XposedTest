19f57bd9aa094b650321a540fee0799a000c4900
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-03-06 17:27:45 +0800

    hook Settings dynamically
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- app/src/main/java/com/example/xposedtest/module/miui/SettingsHook.kt
+++ app/src/main/java/com/example/xposedtest/module/miui/SettingsHook.kt
@@ -7,7 +7,9 @@ import android.view.Menu
 import com.example.xposedtest.annotation.HookClass
 import com.example.xposedtest.annotation.HookMethod
 import com.example.xposedtest.utility.C
+import com.example.xposedtest.utility.callStaticMethod
 import com.example.xposedtest.utility.cast
+import com.example.xposedtest.utility.methodName
 import com.example.xposedtest.xposed.*
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -48,103 +50,35 @@ class SettingsHook(lpparam: XC_LoadPackage.LoadPackageParam) : HookEntry(lpparam
               })
         }
 
-    ClearFragment.`class`()!!
-        .apply {
-          //hook("onOptionsItemSelected", C.MenuItem,
-          //    hookAfter {
-          //      args[0].cast<MenuItem>()?.apply {
-          //        isVisible = false
-          //      }
-          //    })
-
-          //hook("bOZ", hookAfter { result = true })
-
-          hook("onCreateOptionsMenu", C.Menu, C.MenuInflater,
-              hookAfter {
-                /* Difference with getItem? */
-                args[0].cast<Menu>()?.findItem(1)?.isVisible = false
-              })
-        }
+    ClearFragment.`class`()!!.apply {
+      hook("onCreateOptionsMenu", C.Menu, C.MenuInflater, hookAfter {
+        /* Difference with getItem? */
+        args[0].cast<Menu>()?.findItem(1)?.isVisible = false
+      })
+    }
   }
 
   @HookMethod
   private fun hook() {
-    // val cs = MiuiSettingsClass
-    // cs.MiuiMyDeviceSettings.`class`()!!.hook("onPreferenceTreeClick", C.PreferenceScreen, C.Preference, MiuiSettingsHook.onPreferenceTreeClick)
-    // cs.MiuiSettings.`class`()!!.hook("onHeaderClick", C.Header, C.Int, MiuiSettingsHook.onHeaderClick)
-
-    val context = UpgradeContext()
-
-    //"${Settings()["device"][if (is_N()) "s" else "r"]}".`class`()!!
-    //    .hook("onClick",
-    //        C.View,
-    //        hookBoth({
-    //          thisObject.getField<Any>(context.s1)
-    //              ?.run {
-    //                val b = getField<Boolean>(context.s2) ?: return@run
-    //                if (b) {
-    //                  setField(context.s2, false)
-    //                  context.obj = this
-    //                  "Upgrade is disabled!".toast2()
-    //                }
-    //              }
-    //        }, {
-    //          context.obj
-    //              ?.run { setField(context.s2, true) }
-    //       }))
-
-    "${Settings()["development"]["DevelopmentSettings"]}".`class`()!!.let { clazz ->
-      clazz.hook("H",
-          C.Preference,
-          hookBefore {
-            // log("CheckInstance", *args, thisObject.getField("awK"))
-            log("DevelopmentSettings", args[0].cast<Preference>()?.key)
-          })
-
-      // clazz.hook("onCreate",
-      //     C.Bundle,
-      //     hookAfter {
-      //       XposedHelpers.callMethod(thisObject, "findPreference", "debug_debugging_category")
-      //           ?.apply {
-      //             log("removePreference", "awJ")
-      //             XposedHelpers.callMethod(this, "removePreference", thisObject.getField("awJ"))
-      //           }
-      //       XposedHelpers.callMethod(thisObject, "startActivityForResult", Intent("com.miui.securitycenter.action.ADB_INSTALL_VERIFY"), 11)
-      //     })
-    }
-
-
     val a = "${Settings.android["a"]["a"]}".`class`()!!
-
-    a.hook("cEh",
-        C.Context,
-        hookBoth({
-          log("MethodInvoke", "cEh")
-        }, {
-          val b = result.cast<Boolean>() ?: return@hookBoth
-          if (!b) {
-            log("Enable", "adb install")
-            Bundle().apply {
-              putInt("type", 1)
-              putString("key", "security_adb_install_enable")
-              putBoolean("value", true)
-              XposedHelpers.callStaticMethod(a, "cEe", this@hookBoth.args[0], "SET", this)
-            }
-            result = true
-          }
-        }))
-
-    a.hook("cEg",
-        hookBoth({
-          log("MethodInvoke", "cEg")
-        }, {
-          val b = result.cast<Boolean>() ?: return@hookBoth
-          if (!b) {
-            log("Enable", "adb input")
-            XposedHelpers.callStaticMethod(a, "cEi", true)
-            result = true
-          }
-        }))
+    a.apply {
+      val nameOfAdbInstallCheckMethod = this.methodName(C.Boolean, C.Context) ?: return
+      val nameOfSetSecurityPreferenceMethod = this.methodName(C.Bundle, C.Context, C.String, C.Bundle)
+          ?: return
+      val nameOfSetInputEnabledMethod = this.methodName(C.Void, C.Boolean) ?: return
+      log("MethodName", nameOfAdbInstallCheckMethod, nameOfSetSecurityPreferenceMethod, nameOfSetInputEnabledMethod)
+      hook(nameOfAdbInstallCheckMethod, C.Context, hookAfter {
+        log("DevelopmentSetting", "enable adb_install & adb_input")
+        Bundle().apply {
+          putInt("type", 1)
+          putString("key", "security_adb_install_enable")
+          putBoolean("value", true)
+          a.callStaticMethod(nameOfSetSecurityPreferenceMethod, this@hookAfter.args[0], "SET", this)
+        }
+        a.callStaticMethod(nameOfSetInputEnabledMethod, true)
+        result = true
+      })
+    }
 
   }
 
